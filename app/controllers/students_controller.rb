00494635294121400c565e58c532321a80cf2fb4
Josh Morris <jmo0624@gmail.com>
2020-05-19 23:48:53 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,13 +10,7 @@ class StudentsController < ApplicationController
   end
   
   def activate
-    @student = Student.find(params[:id])
-    
-    if @student.active
-      @student.active = false
-    else
-      @student.active = true
-    end
+    set_student
     @student.active = !@student.active
     @student.save
     
